d38624c26bf15358cb39699bb48b552514fb0236
Grzesiek <gzaber@tlen.pl>
2025-02-11 20:33:03 +0100

    refactor: change suspend function to flow

--- app/src/main/java/com/gzaber/weatherapp/data/repository/locations/LocationsRepository.kt
+++ app/src/main/java/com/gzaber/weatherapp/data/repository/locations/LocationsRepository.kt
@@ -1,10 +1,15 @@
 package com.gzaber.weatherapp.data.repository.locations
 
 import com.gzaber.weatherapp.data.repository.locations.model.Location
+import kotlinx.coroutines.flow.Flow
 
 interface LocationsRepository {
+
     suspend fun search(name: String): List<Location>
+
     suspend fun insert(location: Location)
+
     suspend fun delete(location: Location)
-    suspend fun readAll(): List<Location>
+
+    fun observeAll(): Flow<List<Location>>
 }
