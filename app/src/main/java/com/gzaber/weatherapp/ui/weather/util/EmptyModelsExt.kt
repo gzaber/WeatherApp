4acdad41a78250c31d9e0383cb286158fdc1bd7a
Grzesiek <gzaber@tlen.pl>
2025-03-24 22:22:37 +0100

    feat: add other empty models functions

--- app/src/main/java/com/gzaber/weatherapp/ui/weather/util/EmptyModelsExt.kt
+++ app/src/main/java/com/gzaber/weatherapp/ui/weather/util/EmptyModelsExt.kt
@@ -1,24 +1,30 @@
 package com.gzaber.weatherapp.ui.util
 
-import com.gzaber.weatherapp.data.repository.locations.model.Location
+import com.gzaber.weatherapp.data.repository.settings.model.LocationSettings
+import com.gzaber.weatherapp.data.repository.settings.model.WeatherUnitsSettings
 import com.gzaber.weatherapp.data.repository.weather.model.CurrentWeather
 import com.gzaber.weatherapp.data.repository.weather.model.CurrentWeatherParameter
 import com.gzaber.weatherapp.data.repository.weather.model.DailyWeather
 import com.gzaber.weatherapp.data.repository.weather.model.HourlyWeather
 
-fun emptyLocation() = Location(
-    id = "",
-    name = "",
+fun emptyLocationSettings() = LocationSettings(
     latitude = 0.0,
     longitude = 0.0,
+    name = "",
     description = ""
+)
+
+fun emptyWeatherUnitsSettings() = WeatherUnitsSettings(
+    temperatureUnit = "",
+    windSpeedUnit = "",
+    precipitationUnit = ""
 )
 
 fun emptyCurrentWeather() = CurrentWeather(
     weatherCode = 0,
     temperature = CurrentWeatherParameter(0.0, ""),
     humidity = CurrentWeatherParameter(0, ""),
-    rain = CurrentWeatherParameter(0, ""),
+    rain = CurrentWeatherParameter(0.0, ""),
     windSpeed = CurrentWeatherParameter(0.0, ""),
 )
 
