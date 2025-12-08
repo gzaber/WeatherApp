ef3266a77c9cd6fad90c0027aac80ba3f2e51a09
Grzesiek <gzaber@tlen.pl>
2025-12-08 20:37:17 +0100

    chore: update dependencies

--- app/src/main/java/com/gzaber/weatherapp/ui/search/composable/SwipeableLocationListItem.kt
+++ app/src/main/java/com/gzaber/weatherapp/ui/search/composable/SwipeableLocationListItem.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.gzaber.weatherapp.data.repository.locations.model.Location
@@ -17,13 +19,16 @@ fun SwipeableLocationListItem(
     onLocationSwipe: (Location) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val state = rememberSwipeToDismissBoxState()
+
+    LaunchedEffect(state.currentValue) {
+        if (state.currentValue != SwipeToDismissBoxValue.Settled) {
+            onLocationSwipe(location)
+        }
+    }
+
     SwipeToDismissBox(
-        state = rememberSwipeToDismissBoxState(
-            confirmValueChange = {
-                onLocationSwipe(location)
-                true
-            }
-        ),
+        state = state,
         backgroundContent = {
             Box(
                 modifier = Modifier
--- app/src/main/java/com/gzaber/weatherapp/ui/util/DefaultModels.kt
+++ app/src/main/java/com/gzaber/weatherapp/ui/util/DefaultModels.kt
@@ -2,15 +2,6 @@ package com.gzaber.weatherapp.ui.util
 
 import com.gzaber.weatherapp.data.repository.userpreferences.model.LocationPreferences
 import com.gzaber.weatherapp.data.repository.userpreferences.model.WeatherUnitsPreferences
-import com.gzaber.weatherapp.data.repository.weather.model.CurrentWeather
-import com.gzaber.weatherapp.data.repository.weather.model.CurrentWeatherParameter
-import com.gzaber.weatherapp.data.repository.weather.model.DailyWeather
-import com.gzaber.weatherapp.data.repository.weather.model.HourlyWeather
-import com.gzaber.weatherapp.data.repository.weather.model.HumidityUnit
-import com.gzaber.weatherapp.data.repository.weather.model.PrecipitationUnit
-import com.gzaber.weatherapp.data.repository.weather.model.TemperatureUnit
-import com.gzaber.weatherapp.data.repository.weather.model.WeatherCondition
-import com.gzaber.weatherapp.data.repository.weather.model.WindSpeedUnit
 
 fun defaultLocationPreferences() = LocationPreferences(
     latitude = 52.22977,
@@ -23,23 +14,4 @@ fun defaultWeatherUnitsPreferences() = WeatherUnitsPreferences(
     temperatureUnit = "",
     windSpeedUnit = "",
     precipitationUnit = ""
-)
-
-fun defaultCurrentWeather() = CurrentWeather(
-    isDay = true,
-    condition = WeatherCondition.UNKNOWN,
-    temperature = CurrentWeatherParameter(unit = TemperatureUnit.UNKNOWN, value = 0.0),
-    humidity = CurrentWeatherParameter(unit = HumidityUnit.UNKNOWN, 0),
-    windSpeed = CurrentWeatherParameter(unit = WindSpeedUnit.UNKNOWN, 0.0),
-    precipitation = CurrentWeatherParameter(unit = PrecipitationUnit.UNKNOWN, 0.0)
-)
-
-fun defaultHourlyWeather() = HourlyWeather(
-    temperatureUnit = TemperatureUnit.UNKNOWN,
-    hourly = listOf()
-)
-
-fun defaultDailyWeather() = DailyWeather(
-    temperatureUnit = TemperatureUnit.UNKNOWN,
-    daily = listOf()
 )
